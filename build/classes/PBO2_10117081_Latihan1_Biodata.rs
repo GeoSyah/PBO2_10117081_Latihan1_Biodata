2341c352819f85afb205cb23fe4ef2006312f3af
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 13:56:21 +0700

    Add files via upload

---
+++ build/classes/PBO2_10117081_Latihan1_Biodata.rs
@@ -0,0 +1,1 @@
+javaapplication24.PBO2
---
+++ src/PBO2_10117081_Latihan1_Biodata.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan Biodata
+ */
+public class PBO2_10117081_Latihan1_Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Geo Syah Alkautsar");
+        System.out.println("ALAMAT\t\t: Gang Intan VI no.2 Sadang Serang");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 24 September 1998");
+        System.out.println("--------------------------------------");
+        System.out.println("HOBI\t\t: Karate & Gaming");
+        System.out.println("MAKES\t\t: Ramen");
+        System.out.println("MIKES\t\t: Teh");
+    }
+    
+}
